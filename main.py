36666ee2a6ddcfc9bda0394842165817df66dcdc
Warley Almeida <almeida.warley@outlook.com>
2022-04-02 18:52:21 -0400

    Changed main script to support comparator

--- main.py
+++ main.py
@@ -2,28 +2,40 @@ import sys
 import instance as it
 import backward as bd
 import parametric as pd
+import comparator as cp
+
+print('###################### Starting ########################')
 
 problem = it.Instance('instances/{}'.format(sys.argv[1]))
 
-print('--------------------- Backward -----------------------')
+print('\n--------------------- Backward -----------------------')
 
 backward_solver = bd.Backward(problem)
 backward_solver.run_solver()
 backward_solver.print_policy()
 
-_ = input('Press enter to move forward')
+print('\n--------------------- Backward -----------------------')
 
-print('-------------------- Parametric ----------------------')
+print('\n-------------------- Parametric ----------------------')
 
 parametric_solver = pd.Parametric(problem)
 parametric_solver.train_solver()
 parametric_solver.run_solver()
 parametric_solver.print_policy()
 
-_ = input('Press enter to move forward')
+print('\n-------------------- Parametric ----------------------')
 
-print('******************** Technical ***********************')
+print('\n-------------------- Comparator ----------------------')
+
+comparator = cp.Comparator(problem, backward_solver, parametric_solver)
+comparator.run_comparison()
+
+print('\n-------------------- Comparator ----------------------')
+
+print('\n******************** Technical ***********************')
 
 parametric_solver.show_parameters()
 
-_ = input('Press enter to move forward')
+print('\n******************** Technical ***********************')
+
+print('\n##################### Finishing #######################')
