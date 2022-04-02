f8866096cae2bd36a6c6f7e7cb797837d58ed771
Warley Almeida <almeida.warley@outlook.com>
2022-04-02 12:20:49 -0400

    Added a pretty structure for the main script

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import sys
 import instance as it
 import backward as bd
+import parametric as pd
 
 problem = it.Instance('instances/{}'.format(sys.argv[1]))
 
-solver = bd.Backward(problem)
-solver.run_solver()
-solver.print_policy()
+print('--------------------- Backward -----------------------')
+
+backward_solver = bd.Backward(problem)
+backward_solver.run_solver()
+backward_solver.print_policy()
+
+_ = input('Press enter to move forward')
+
+print('-------------------- Parametric ----------------------')
+
+parametric_solver = pd.Parametric(problem)
+parametric_solver.train_solver()
+parametric_solver.run_solver()
+parametric_solver.print_policy()
+
+_ = input('Press enter to move forward')
+
+print('******************** Technical ***********************')
+
+parametric_solver.show_parameters()
+
+_ = input('Press enter to move forward')
