82861d3b81484cdf1509ae20e985ddbf6397bf2a
Warley Almeida <almeida.warley@outlook.com>
2022-04-05 22:53:15 -0400

    Added a few experiment scripts to generate policies

--- decay_graph.py
+++ decay_graph.py
@@ -1,15 +1,19 @@
+import argparse as ag
 import instance as it
 import backward as bd
 import parametric as pd
 import matplotlib.pyplot as plt
 
-folder = 'instances/large'
-method = 'backward'
-samples = 500
+parser = ag.ArgumentParser(description = 'Create graphs with varying rationality decay parameter for an instance of the multi-period competitive facility location problem of temporary retail faciltiies')
+parser.add_argument('folder', type = str, help = 'Path to the folder containing files with the instance information')
+parser.add_argument('solver', type = str, help = 'Name of the solver used to generate the optimal policy')
+parser.add_argument('max_decay', type = int, help = 'Maximum value for the rationality decay parameter')
+parser.add_argument('-s', '--samples', type = int, help = 'Set number of samples of the random variable (0 means complete enumeration)', default = 0)
+arguments = parser.parse_args()
 
-problem = it.Instance(folder, samples, samples)
+problem = it.Instance(arguments.folder, arguments.samples, 1)
 
-ds = list(range(2, 5))
+ds = [1, 1.2, 1.4, 1.6, 1.8] + list(range(2, arguments.max_decay + 1))
 
 bJs = {}
 
@@ -20,7 +24,7 @@ for d in ds:
 
     problem.d = d
 
-    if method == 'parametric':
+    if arguments.solver == 'parametric':
         solver = pd.Parametric(problem)
         solver.train_solver()
     else:
@@ -47,17 +51,17 @@ color = {
 }
 
 for x in problem.X:
-    if method == 'parametric':
+    if arguments.solver == 'parametric':
         plt.plot(ds, bJs[x], '-x', label = 'J̃_0({})'.format(x), color = color[x]) #, linewidth = 4, markersize = 10, zorder = 1)
     else:
         plt.plot(ds, bJs[x], '-x', label = 'J_0({})'.format(x), color = color[x]) #, linewidth = 4, markersize = 10, zorder = 1)
 
 plt.xlabel('Rationality decay parameter d')
-if method == 'parametric':
+if arguments.solver == 'parametric':
     plt.ylabel('Approximate cost-to-go function J̃_0(x)')
 else:
     plt.ylabel('Optimal cost-to-go function J_0(x)')
 plt.legend(loc = 'upper right')
-plt.savefig('{}_{}_decay.png'.format(method, folder.replace('instances/', '')))
+plt.savefig('graphs/{}_{}_decay.png'.format(arguments.solver, problem.name))
 
 plt.close()
--- experiment.sh
+++ experiment.sh
@@ -1,9 +1,3 @@
-# Backward solver, varying rationality decay for instance large with s = 0
-#for i in {2..10}
-#do
-#    python main.py instances/large --backward --export -d $i -s 100
-#done
-
 # Backward solver, varying number of samples for instance large with d = 2
 for i in 10 20 30 40 50 60 70 80 90 100 120 140 160 180 200
 do
@@ -21,3 +15,14 @@ do
         python main.py instances/large --parametric --export -d 2 -s $i
     done
 done
+
+# Parametric solver, varying number of samples for instance medium with d = 2
+for i in {1..15}
+do
+    for j in {1..100}
+    do
+        python main.py instances/medium --parametric --export -d 2 -s $i
+    done
+done
+
+python main.py instances/large --backward --export -d 2 -s 0
