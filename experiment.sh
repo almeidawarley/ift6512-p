53b5a79e69cd65eeda3386b9ddc7567773060949
Warley Almeida <almeida.warley@outlook.com>
2022-04-06 23:13:23 -0400

    Organized experiments in one single .sh file

--- experiment.sh
+++ experiment.sh
@@ -1,4 +1,27 @@
-# Backward solver, varying number of samples for instance large with d = 2
+# List of experiments ran for the results in the report
+
+# Compute (full) backward policy for instance medium
+python main.py instances/medium --backward --export -d 2
+# Compute (sampled) backward policy for instance large
+python main.py instances/large --backward --export -d 2 -s 512
+
+# Compute (full) parametric policy for instance large
+python main.py instances/medium --parametric --export -d 2
+# Compute (sampled) parametric policy for instance large
+python main.py instances/large --parametric --export -d 2 -s 512
+
+# Compute (sampled) backward solver for parameter d = 2
+# Varying number of samples for graphs of question #3
+for i in {1..15}
+do
+    for j in {1..100}
+    do
+        python main.py instances/medium --backward --export -d 2 -s $i
+    done
+done
+
+# Compute (sampled) backward solver for parameter d = 2
+# Varying number of samples for graphs of question #3
 for i in 10 20 30 40 50 60 70 80 90 100 120 140 160 180 200
 do
     for j in {1..10}
@@ -7,22 +30,12 @@ do
     done
 done
 
-# Parametric solver, varying number of samples for instance large with d = 2
-for i in 10 20 30 40 50 60 70 80 90 100 120 140 160 180 200
-do
-    for j in {1..100}
-    do
-        python main.py instances/large --parametric --export -d 2 -s $i
-    done
-done
-
-# Parametric solver, varying number of samples for instance medium with d = 2
-for i in {1..15}
-do
-    for j in {1..100}
-    do
-        python main.py instances/medium --parametric --export -d 2 -s $i
-    done
-done
-
-python main.py instances/large --backward --export -d 2 -s 0
+# Compute graphs shown in the computational experiments
+# For instance medium, use full enumeration as reference
+python sample_graph.py instances/medium -d 2
+python bar_graph.py instances/medium -d 2
+python decay_graph.py instances/medium 50 -d 2
+# For instance large, use s= 512 samples as reference
+python sample_graph.py instances/large -d 2 -s 512
+python boxplot_graph.py instances/large -d 2 -s 512
+python decay_graph.py instances/large 50 -d 2 -s 512
