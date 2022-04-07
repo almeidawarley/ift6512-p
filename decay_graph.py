7e8df2885699f937ca4f90a4102706262c4612e1
Warley Almeida <almeida.warley@outlook.com>
2022-04-06 23:33:25 -0400

    Updated script to generate decay graph

--- decay_graph.py
+++ decay_graph.py
@@ -1,41 +1,42 @@
+import matplotlib.pyplot as plt
 import argparse as ag
 import instance as it
 import backward as bd
-import parametric as pd
-import matplotlib.pyplot as plt
 
-parser = ag.ArgumentParser(description = 'Create graphs with varying rationality decay parameter for an instance of the multi-period competitive facility location problem of temporary retail faciltiies')
-parser.add_argument('folder', type = str, help = 'Path to the folder containing files with the instance information')
-parser.add_argument('solver', type = str, help = 'Name of the solver used to generate the optimal policy')
+# Parse arguments using argparse
+parser = ag.ArgumentParser(description = 'Draw graph with varying values of the rationality decay parameter for some instance')
+parser.add_argument('folder', type = str, help = 'Path to the folder with instance files')
 parser.add_argument('max_decay', type = int, help = 'Maximum value for the rationality decay parameter')
-parser.add_argument('-s', '--samples', type = int, help = 'Set number of samples of the random variable (0 means complete enumeration)', default = 0)
+parser.add_argument('-s', '--samples', type = int, help = 'Set number of samples (0 means full enumeration)', default = 0)
 arguments = parser.parse_args()
 
+# Create problem object with d = 1
 problem = it.Instance(arguments.folder, arguments.samples, 1)
 
+# Create list of values of parameter d
 ds = [1, 1.2, 1.4, 1.6, 1.8] + list(range(2, arguments.max_decay + 1))
 
+# Create dictionary to store J_0(x) values
+# The list has values for d = 1, ..., max_decay
 bJs = {}
-
 for x in problem.X:
     bJs[x] = []
 
+# Loop over values of parameter d
 for d in ds:
 
+    # Set parameter d in the instance
     problem.d = d
+    # Run the backward solver
+    solver = bd.Backward(problem)
+    solver.run_solver()
 
-    if arguments.solver == 'parametric':
-        solver = pd.Parametric(problem)
-        solver.train_solver()
-    else:
-        solver = bd.Backward(problem)
-
-    solver.run_solver(True)
-
+    # Store the J_0(x) values
     k = 0
     for x in problem.X:
         bJs[x].append(solver.J(k, x))
 
+# Set colors for up to 11 states
 color = {
     '0': 'red',
     '1': 'blue',
@@ -50,18 +51,13 @@ color = {
     '10': 'black'
 }
 
+# Plot J_0(x) values for each state x
 for x in problem.X:
-    if arguments.solver == 'parametric':
-        plt.plot(ds, bJs[x], '-x', label = 'J̃_0({})'.format(x), color = color[x]) #, linewidth = 4, markersize = 10, zorder = 1)
-    else:
-        plt.plot(ds, bJs[x], '-x', label = 'J_0({})'.format(x), color = color[x]) #, linewidth = 4, markersize = 10, zorder = 1)
+    plt.plot(ds, bJs[x], '-x', label = 'J_0({})'.format(x), color = color[x]) #, linewidth = 4, markersize = 10, zorder = 1)
 
+# Set other graph settings
 plt.xlabel('Rationality decay parameter d')
-if arguments.solver == 'parametric':
-    plt.ylabel('Approximate cost-to-go function J̃_0(x)')
-else:
-    plt.ylabel('Optimal cost-to-go function J_0(x)')
+plt.ylabel('Optimal cost-to-go function J_0(x)')
 plt.legend(loc = 'upper right')
-plt.savefig('graphs/{}_{}_decay.png'.format(arguments.solver, problem.name))
-
+plt.savefig('graphs/decay_{}.png'.format(problem.name))
 plt.close()
