6dab14fc5fe092f19ab2712435377ebba2d0dd21
Warley Almeida <almeida.warley@outlook.com>
2022-04-07 00:04:35 -0400

    Fixed a typo in the commands for experiments

--- experiment.sh
+++ experiment.sh
@@ -34,8 +34,8 @@ done
 # For instance medium, use full enumeration as reference
 python sample_graph.py instances/medium -d 2
 python bar_graph.py instances/medium -d 2
-python decay_graph.py instances/medium 50 -d 2
+python decay_graph.py instances/medium 50
 # For instance large, use s= 512 samples as reference
 python sample_graph.py instances/large -d 2 -s 512
 python boxplot_graph.py instances/large -d 2 -s 512
-python decay_graph.py instances/large 50 -d 2 -s 512
+python decay_graph.py instances/large 50 -s 512
