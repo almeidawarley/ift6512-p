842d0c420463f0300dd9c1c6a9a34840c4826b6e
Warley Almeida <almeida.warley@outlook.com>
2022-04-07 20:21:38 -0400

    Removed experiments that did not go into the report

--- experiment.sh
+++ experiment.sh
@@ -20,26 +20,6 @@ python main.py instances/medium --parametric --export -d 2
 # Compute (sampled) parametric policy for instance large
 python main.py instances/large --parametric --export -d 2 -s 512
 
-# Compute (sampled) backward solver for parameter d = 2
-# Varying number of samples for graphs of question #3
-for i in {1..15}
-do
-    for j in {1..100}
-    do
-        python main.py instances/medium --backward --export -d 2 -s $i
-    done
-done
-
-# Compute (sampled) backward solver for parameter d = 2
-# Varying number of samples for graphs of question #3
-for i in 10 20 30 40 50 60 70 80 90 100 120 140 160 180 200
-do
-    for j in {1..100}
-    do
-        python main.py instances/large --backward --export -d 2 -s $i
-    done
-done
-
 # Compute graphs shown in the computational experiments
 # For instance medium, use full enumeration as reference
 python sample_graph.py instances/medium -d 2
